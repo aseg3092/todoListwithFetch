a6ad711c26b01a86f38820d77a2d599a591472f0
aseg3092 <gabrielsalazar3092@gmail.com>
2022-01-31 23:28:49 +0000

    feat: agregando saveTodo y boton delete

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -4,32 +4,20 @@ import TodoList from "./TodoList.jsx";
 //create your first component
 const Home = () => {
 	const [inputTodo, setInputTodo] = useState("");
-	const todos = [];
+	const [items, setItems] = useState([]);
 
 	const saveTodo = (e) => {
+		// e.preventDefault();
 		if (e.key === "Enter") {
-			//console.log({ inputTodo }.inputTodo);
-			todos.push({ inputTodo }.inputTodo);
-			console.log(todos[0]);
-			document.getElementById("inputTodo").value = "";
+			setItems([...items, { id: items.length, name: inputTodo }]);
+			setInputTodo("");
+			console.log("un funciona");
 		}
 	};
 
 	return (
 		<div>
 			<h1 className="text-center mt-5">TODOS</h1>
-			{/* <form>
-				<div className="mb-3">
-					<input
-						type="text"
-						placeholder="Add to do here"
-						className="form-control"
-						id="inputTodo"
-						aria-describedby="emailHelp"
-						onChange={(e) => setInputTodo(e.target.value)}
-					/>
-				</div>
-			</form> */}
 			<input
 				type="text"
 				placeholder="Add to do here"
@@ -39,7 +27,16 @@ const Home = () => {
 				onChange={(e) => setInputTodo(e.target.value)}
 				onKeyPress={saveTodo}
 			/>
-			<TodoList inputTodo={todos} />
+
+			<ul>
+				{items.map((item) => (
+					<>
+						<li key={item.id}>{item.name}</li>
+						<i className="fa fa-trash"></i>
+					</>
+				))}
+			</ul>
+			{/* <TodoList inputTodo={todos} /> */}
 		</div>
 	);
 };
